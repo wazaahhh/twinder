fdf9b5a115eb51f7499fbe5c905b6d8dfa4006dc
Matthieu Nowicki <nowicki.matthieu>
2014-06-17 17:32:51 -0700

    Update model

--- twinder2/models.py
+++ twinder2/models.py
@@ -5,7 +5,7 @@ class Serie(models.Model):
 	right = models.BooleanField(default=False)
 	created_at = models.DateTimeField(auto_now_add=True, editable=True, blank=True, null=True)
 
-class Person(models.Model):
+class Person_new(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     object_id = models.IntegerField(max_length=100, blank=True, null=True)
