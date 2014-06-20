6db11be537bad3485bd3be4f1544141021888a81
Matthieu Nowicki <nowicki.matthieu>
2014-06-19 17:25:07 -0700

    Db call - write

--- twinder2/models.py
+++ twinder2/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-class Serie(models.Model):
+class UnUser(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    user_name = models.CharField(max_length=30)
+
+class UneSerie(models.Model):
 	left = models.BooleanField(default=False)
 	right = models.BooleanField(default=False)
 	created_at = models.DateTimeField(auto_now_add=True, editable=True, blank=True, null=True)
-
-class Person_new(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    object_id = models.IntegerField(max_length=100, blank=True, null=True)
-    serie = models.ForeignKey(Serie, related_name='serie', blank=True, null=True)
+	user = models.ForeignKey(UnUser)
